c8af0d25534d64d6e279b3ceffb0628e539c9ceb
xiaolv125 <15636331627@163.com>
2017-12-10 15:16:43 +0800

    require练习

---
+++ require/add.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/12/10.
+ */
+function add(a,b) {
+    return a+b;
+}
---
+++ require/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/12/10.
+ */
+require(['sortArray',function (sortArray) {
+    var arr=[1,4,5,2,3,9,8];
+    var num=43;
+    var str="nihao";
+    console.log(sortArray(arr));
+}])
+
+define(['sortArray'],function (sortArray) {
+    var arr=[1,4,5,2,3,9,8];
+    var num=43;
+    var str="nihao";
+    console.log(sortArray(arr));
+});
+
+
+
