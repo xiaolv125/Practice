abd6f45e47962525c473d167162d00514870ce15
xiaolv125 <15636331627@163.com>
2017-12-23 15:00:16 +0800

    轮播图模块

--- require/轮播图模块/Carousel.js
+++ require/轮播图模块/Carousel.js
@@ -7,7 +7,25 @@ define(function () {
         this.$imgBox=$('<div class="carousel-img-box"></div>');
         this.$tab=$('<ul class="carousel-tab"></ul>');
         this.$prev=$('<span class="carousel-prev">&lt;</span>');
-        this.$next=$('<span class="carousel-next">&gt;</span>')
+        this.$next=$('<span class="carousel-next">&gt;</span>');
+        this.defaultOption={
+            buttonType:'square',
+            btnPos:'bottom',
+            speed:1000
+        }
+    }
+    Carousel.prototype.init=function (option) {
+        $.extend(this.defaultOption,option);
+        console.log(this.defaultOption);
+        this.$container.append(this.$imgBox).append(this.$tab)
+            .append(this.$prev).append(this.$next);
+
+        for(var i=0;i<this.defaultOption.imgDate.length;i++){
+            var cls=(i==0)?"seleted":"";
+            this.$imgBox.append($('<img class="'+cls+'" src="'+this.defaultOption.imgDate[i]+'"/>'));
+            this.$tab.append($('<li class="'+cls+'">'+(i+1)+'</li>'))
+        }
+        $(this.defaultOption.selector).append(this.$container);
     }
     return Carousel;
 })
