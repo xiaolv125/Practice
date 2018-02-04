2443d9561c1173762ae40591b6787442402dee0f
xiaolv125 <15636331627@163.com>
2018-02-04 20:42:23 +0800

    app

--- app/src/components/books/BookList.vue
+++ app/src/components/books/BookList.vue
@@ -1,24 +1,47 @@
 <template>
     <div>
-        
-        <h2>Book</h2>
-        <h2>Book</h2>
-        <h2>Book</h2>
-        <h2>Book</h2>
-        
+        <swipe class="my-swipe">
+            <swipe-item class="slide1"><img src="@/assets/images/xiaowangzi.jpg" alt=""></swipe-item>
+            <swipe-item class="slide2"><img src="@/assets/images/touyingzi.jpg" alt=""></swipe-item>
+            <swipe-item class="slide3"><img src="@/assets/images/zhuifengzheng.jpg" alt=""></swipe-item>
+            <swipe-item class="slide4"><img src="@/assets/images/buweijianglai.jpg" alt=""></swipe-item>
+        </swipe>
     </div>
 </template>
 
 <script>
-    
+    import vueswipe from 'vue-swipe/dist/vue-swipe.css';
+    import { Swipe, SwipeItem } from 'vue-swipe';
     export default {
         mounted:function(){
             this.$store.commit("change",{bgColor:"rgb(139, 121, 14)",title:"Book"});
+        },
+        components:{
+            Swipe,
+            SwipeItem
         }
         
     }
 </script>
 
 <style scoped>
+.my-swipe {
+height: 8rem;
+}
+ 
+/* .slide1 {
+  background-image: url('./')
+}
+ 
+.slide2 {
+  background-image:url('./');
+}
+ 
+.slide3 {
+  background-image:url('./');
+} */
+/* .slide4 {
+  background-image:url('./@/assets/images/buweijianglai.jpg');
+} */
 
 </style>
